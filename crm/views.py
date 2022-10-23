e084a3b4d347d5dca3c421f7ea3205581390dc94
varunsaikethineni <111914809+varunsaikethineni@users.noreply.github.com>
2022-10-22 22:48:00 -0500

    code

---
+++ crm/views.py
@@ -0,0 +1,221 @@
+import csv
+
+from django.db.models import Sum
+from _decimal import Decimal
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.template.loader import get_template
+
+from .models import *
+from .forms import *
+
+now = timezone.now()
+
+
+def home(request):
+    return render(request, 'crm/home.html',
+                  {'crm': home})
+
+
+@login_required
+def customer_list(request):
+    customer = Customer.objects.filter()
+    return render(request, 'crm/customer_list.html',
+                  {'customers': customer})
+
+
+@login_required
+def customer_edit(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == "POST":
+        # update
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.updated_date = timezone.now()
+            customer.save()
+            customer = Customer.objects.filter()
+            return render(request, 'crm/customer_list.html',
+                          {'customers': customer})
+    else:
+        # edit
+        form = CustomerForm(instance=customer)
+    return render(request, 'crm/customer_edit.html', {'form': form})
+
+
+@login_required
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customer.delete()
+    return redirect('crm:customer_list')
+
+
+@login_required
+def service_list(request):
+    services = Service.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'crm/service_list.html', {'services': services})
+
+
+@login_required
+def service_new(request):
+    if request.method == "POST":
+        form = ServiceForm(request.POST)
+        if form.is_valid():
+            service = form.save(commit=False)
+            service.created_date = timezone.now()
+            service.save()
+            services = Service.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'crm/service_list.html',
+                          {'services': services})
+    else:
+        form = ServiceForm()
+
+    return render(request, 'crm/service_new.html', {'form': form})
+
+
+@login_required
+def service_edit(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    if request.method == "POST":
+        form = ServiceForm(request.POST, instance=service)
+        if form.is_valid():
+            service = form.save()
+            service.updated_date = timezone.now()
+            service.save()
+            services = Service.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'crm/service_list.html', {'services': services})
+    else:
+        form = ServiceForm(instance=service)
+    return render(request, 'crm/service_edit.html', {'form': form})
+
+
+@login_required
+def service_delete(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    service.delete()
+    return redirect('crm:service_list')
+
+
+@login_required()
+def product_list(request):
+    products = Product.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'crm/product_list.html', {'products': products})
+
+
+@login_required
+def product_new(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.created_date = timezone.now()
+            product.save()
+            products = Product.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'crm/product_list.html',
+                          {'products': products})
+    else:
+        form = ProductForm()
+
+    return render(request, 'crm/product_new.html', {'form': form})
+
+
+@login_required
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            product = form.save()
+            product.updated_date = timezone.now()
+            product.save()
+            products = Product.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'crm/product_list.html', {'products': products})
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'crm/product_edit.html', {'form': form})
+
+
+@login_required
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product.delete()
+    return redirect('crm:product_list')
+
+
+@login_required
+def summary(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    services = Service.objects.filter(cust_name=pk)
+    products = Product.objects.filter(cust_name=pk)
+    sum_service_charge = \
+        Service.objects.filter(cust_name=pk).aggregate(Sum('service_charge'))
+    sum_product_charge = \
+        Product.objects.filter(cust_name=pk).aggregate(Sum('charge'))
+
+    # if no product or service records exist for the customer,
+    # change the ‘None’ returned by the query to 0.00
+    sum = sum_product_charge.get("charge__sum")
+    if sum == None:
+        sum_product_charge = {'charge__sum': Decimal('0')}
+    sum = sum_service_charge.get("service_charge__sum")
+    if sum == None:
+        sum_service_charge = {'service_charge__sum': Decimal('0')}
+
+    return render(request, 'crm/summary.html', {'customer': customer,
+                                                'products': products,
+                                                'services': services,
+                                                'sum_service_charge': sum_service_charge,
+                                                'sum_product_charge': sum_product_charge, })
+
+
+# Generate CSV file
+def export_csv(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    services = Service.objects.filter(cust_name=pk)
+    products = Product.objects.filter(cust_name=pk)
+    sum_service_charge = \
+        Service.objects.filter(cust_name=pk).aggregate(Sum('service_charge'))
+    sum_product_charge = \
+        Product.objects.filter(cust_name=pk).aggregate(Sum('charge'))
+
+    # if no product or service records exist for the customer,
+    # change the ‘None’ returned by the query to 0.00
+    sum = sum_product_charge.get("charge__sum")
+    if sum == None:
+        sum_product_charge = {'charge__sum': Decimal('0')}
+    sum = sum_service_charge.get("service_charge__sum")
+    if sum == None:
+        sum_service_charge = {'service_charge__sum': Decimal('0')}
+    context = {'customer': customer,
+               'products': products,
+               'services': services,
+               'sum_service_charge': sum_service_charge,
+               'sum_product_charge': sum_product_charge, }
+    total = Decimal(sum_service_charge.get('service_charge__sum')) + Decimal(sum_product_charge.get('charge__sum'))
+    templatepath = 'csv/report.html'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename="report.csv"'
+    template = get_template(templatepath)
+    writer = csv.writer(response)
+    writer.writerow(["Customer Summary"])
+    writer.writerow(["Customer Name ", customer.cust_name])
+    writer.writerow(["Total Of Service Charges and Product Charges ", total])
+    writer.writerow(["Services Information"])
+    writer.writerow(["Service Category", "Description", "Location", "Setup Time", "Cleanup Time", "Service Charge"])
+    queryset = Service.objects.filter(cust_name=pk)
+    for row in queryset:
+        writer.writerow(
+            [row.service_category, row.description, row.location, row.setup_time, row.cleanup_time, row.service_charge])
+    writer.writerow(["Total Service Charges", sum_service_charge.get("service_charge__sum")])
+
+    writer.writerow(["Product Information"])
+    writer.writerow(["Product", "Description", "Quantity", "Pickup Time", "Total Charge"])
+    queryset = Product.objects.filter(cust_name=pk)
+    for row in queryset:
+        writer.writerow(
+            [row.product, row.description, row.quantity, row.pickup_time, row.charge])
+    writer.writerow(["Total Product Charges", sum_product_charge.get("charge__sum")])
+
+    return response
